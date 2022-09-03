ebd1931c611940ecf714428a4d3f42caa6019f21
expglsf <expglsf@foxmail.com>
2022-09-03 10:36:16 +0800

    add graphics base interface

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+
+namespace OctoPotato {
+    class GraphicsManager : implements IRuntimeModule {
+    public:
+        virtual ~GraphicsManager() {}
+    };
+}
